889301300119bc697d65a8733e1db363fe28b923
IvanNik17 <30436041+IvanNik17@users.noreply.github.com>
2020-08-20 15:22:06 +0200

    Code and meshes

---
+++ Assets/mouseOrbit.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+[AddComponentMenu("Camera-Control/Mouse Orbit with zoom")]
+public class mouseOrbit : MonoBehaviour {
+ 
+    public Transform target;
+    public Transform camera;
+
+    public GameObject light_front;
+    public GameObject light_left;
+    public GameObject light_right;
+
+
+    public float intensityModifier = 0.7f;
+
+
+    public float distance = 5.0f;
+    public float xSpeed = 120.0f;
+    public float ySpeed = 120.0f;
+ 
+    public float yMinLimit = -20f;
+    public float yMaxLimit = 80f;
+ 
+    public float distanceMin = .5f;
+    public float distanceMax = 15f;
+ 
+    private Rigidbody rigidbody;
+ 
+    float x = 0.0f;
+    float y = 0.0f;
+
+    int lightNum = 1;
+ 
+    // Use this for initialization
+    void Start () 
+    {
+        Vector3 angles = camera.eulerAngles;
+        x = angles.y;
+        y = angles.x;
+
+        rigidbody = camera.GetComponent<Rigidbody>();
+ 
+        // Make the rigid body not change rotation
+        if (rigidbody != null)
+        {
+            rigidbody.freezeRotation = true;
+        }
+
+        light_left.SetActive(false);
+        light_right.SetActive(false);
+        
+    }
+ 
+    void LateUpdate () 
+    {
+        if (target && Input.GetMouseButton(1)) 
+        {
+            x += Input.GetAxis("Mouse X") * xSpeed * distance * 0.02f;
+            y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+ 
+            y = ClampAngle(y, yMinLimit, yMaxLimit);
+ 
+            Quaternion rotation = Quaternion.Euler(y, x, 0);
+ 
+            distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel")*2, distanceMin, distanceMax);
+
+            light_front.GetComponent<Light>().intensity = intensityModifier * (distance + .0001f);
+            light_left.GetComponent<Light>().intensity = intensityModifier * (distance + .0001f);
+            light_right.GetComponent<Light>().intensity = intensityModifier * (distance + .0001f);
+
+
+ 
+            //RaycastHit hit;
+            //if (Physics.Linecast(target.position, camera.position, out hit)) 
+            //{
+            //    distance -=  hit.distance;
+            //}
+            Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
+            Vector3 position = rotation * negDistance + target.position;
+
+            camera.rotation = rotation;
+            camera.position = position;
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            lightNum += 1;
+
+            if (lightNum > 3)
+            {
+                lightNum = 1;
+            }
+
+            if (lightNum == 1)
+            {
+                light_front.SetActive(true);
+                light_left.SetActive(false);
+                light_right.SetActive(false);
+
+            }
+            else if(lightNum == 2)
+            {
+                light_front.SetActive(false);
+                light_left.SetActive(true);
+                light_right.SetActive(false);
+            }
+            else if (lightNum == 3)
+            {
+                light_front.SetActive(false);
+                light_left.SetActive(false);
+                light_right.SetActive(true);
+            }
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.KeypadPlus))
+        {
+            target.transform.localScale *= 1.1f;
+        }
+        else if (Input.GetKeyDown(KeyCode.KeypadMinus))
+        {
+            target.transform.localScale *= 0.9f;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            intensityModifier += 0.1f;
+
+            light_front.GetComponent<Light>().intensity = intensityModifier * (distance + .0001f);
+            light_left.GetComponent<Light>().intensity = intensityModifier * (distance + .0001f);
+            light_right.GetComponent<Light>().intensity = intensityModifier * (distance + .0001f);
+        }
+        else if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            intensityModifier -= 0.1f;
+
+            light_front.GetComponent<Light>().intensity = intensityModifier * (distance + .0001f);
+            light_left.GetComponent<Light>().intensity = intensityModifier * (distance + .0001f);
+            light_right.GetComponent<Light>().intensity = intensityModifier * (distance + .0001f);
+        }
+
+    }
+ 
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+            angle += 360F;
+        if (angle > 360F)
+            angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
+    }
+}
---
+++ Assets/verticesAsBilboards.cs
@@ -0,0 +1,279 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.IO;
+
+public class verticesAsBilboards : MonoBehaviour {
+
+    public GameObject target;
+
+    public GameObject cameraMain;
+
+    public Vector2 brushThresh = new Vector2(0, 10f);
+
+    int[] triangles;
+    MeshFilter mf;
+    Vector3[] vertices;
+    Color[] vertColors;
+
+    int[] vertLabel;
+
+
+
+    bool[] selectedVerts;
+
+
+    bool[] isChecked;
+
+    bool addRemove = true;
+
+    int selectLabel = 1;
+
+    float brushSize = 0f;
+
+    Texture2D tex;
+
+    
+
+
+	// Use this for initialization
+	void Start () {
+
+        GameObject bilboardHolder = GameObject.Find("BilboardHolder");
+
+        mf = target.GetComponent<MeshFilter>();
+
+        triangles = mf.mesh.triangles;
+        vertices = mf.mesh.vertices;
+        vertColors = new Color[vertices.Length];
+
+        vertLabel = new int[vertices.Length];
+
+        selectedVerts = new bool[vertices.Length];
+
+        isChecked = new bool[vertices.Length];
+
+
+        //tex = new Texture2D((int)(imageDim[0] * imageScale), (int)(imageDim[1] * imageScale), TextureFormat.RGB24, false);
+
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertColors[i] = new Color(1f, 1f, 1f);
+
+            vertLabel[i] = 0;
+
+            selectedVerts[i] = false;
+
+            
+
+        }
+
+	}
+
+
+    Color getLabelColor(int label)
+    {
+        Color outputColor = Color.white;
+        switch (label)
+        {
+            case 0:
+                outputColor = Color.white;
+                break;
+            case 1:
+                outputColor = Color.red;
+                break;
+            case 2:
+                outputColor = Color.green;
+                break;
+            case 3:
+                outputColor = Color.blue;
+                break;
+            default:
+                outputColor = Color.white;
+                break;
+        }
+
+        return outputColor;
+    }
+	
+	// Update is called once per frame
+
+
+    void Update()
+    {
+        Ray test1 = cameraMain.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+
+        Debug.DrawRay(test1.origin, test1.direction * 10, Color.red);
+        if (Input.GetMouseButton(0))
+        {
+            //Ray ray = cameraMain.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+            //RaycastHit hit;
+
+
+
+
+            Vector3 currMouse = Input.mousePosition;
+
+            for (float i = currMouse.x - brushSize; i <= currMouse.x + brushSize; i++)
+            {
+                for (float j = currMouse.y - brushSize; j <= currMouse.y + brushSize; j++)
+                {
+
+                    Ray ray = cameraMain.GetComponent<Camera>().ScreenPointToRay(new Vector3(i,j,0f));
+                    RaycastHit hit;
+
+                    if (Physics.Raycast(ray, out hit))
+                    {
+
+
+                        for (int m = 0; m < 3; m++)
+                        {
+
+
+
+
+                            var test = triangles[hit.triangleIndex * 3 + m];
+
+                            if (addRemove)
+                            {
+
+                                vertColors[test] = getLabelColor(selectLabel);
+
+                                vertLabel[test] = selectLabel;
+                            }
+                            else
+                            {
+                                vertColors[test] = Color.white;
+                            }
+
+
+
+                        }
+
+
+
+                    }
+
+                }
+            }
+            
+
+            //if (Physics.Raycast(ray, out hit))
+            //{
+
+            //    int numCheck = 0;
+            //    if (selectFaces)
+            //    {
+            //        numCheck = 3;
+            //    }
+            //    else
+            //    {
+            //        numCheck = 1;
+            //    }
+
+            //    for (int m = 0; m < numCheck; m++)
+            //    {
+
+                    
+
+
+            //        var test = triangles[hit.triangleIndex * 3 + m];
+
+            //        if (addRemove)
+            //        {
+
+            //            vertColors[test] = getLabelColor(selectLabel);
+
+            //            vertLabel[test] = selectLabel;
+            //        }
+            //        else
+            //        {
+            //            vertColors[test] = Color.white;
+            //        }
+                    
+
+
+            //    }
+
+               
+
+            //}
+
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftBracket))
+        {
+
+
+            brushSize = Mathf.Clamp(brushSize - 0.5f, brushThresh[0], brushThresh[1]);
+
+            Debug.Log(brushSize);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightBracket))
+        {
+
+
+            brushSize = Mathf.Clamp(brushSize + 0.5f, brushThresh[0], brushThresh[1]);
+            Debug.Log(brushSize);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            addRemove = !addRemove;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            selectLabel = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            selectLabel = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            selectLabel = 3;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            
+            StreamWriter streamWriter = new StreamWriter("Assets\\labeledPointCloud.txt");
+            string output = "";
+
+            for (int i = 0; i < vertLabel.Length; i++)
+            {
+                output = vertLabel[i].ToString();
+
+                streamWriter.WriteLine(output);
+
+            }
+
+            streamWriter.Close();
+
+            Debug.Log("SAVED");
+        }
+
+
+
+        //if (Input.GetMouseButtonUp(0))
+        //{
+        //    isChecked = new bool[vertices.Length];
+        //}
+        mf.mesh.colors = vertColors;
+
+
+
+       
+
+
+    }
+
+
+}
